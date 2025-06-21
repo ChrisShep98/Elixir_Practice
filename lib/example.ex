708ecbe472471ca51953b38b3fe4d030e04c2f09
ChrisShep98 <shepardchristopher1@gmail.com>
2025-06-21 15:10:26 -0400

    atom and conditonal example

--- lib/example.ex
+++ lib/example.ex
@@ -7,13 +7,21 @@ defmodule Example do
 
   def start(_type, _args) do
     Example.main()
-    IO.puts(:"Hello World")
     Supervisor.start_link([], strategy: :one_for_one)
   end
 
   def main do
     x = 5
-    IO.puts(x)
+    # IO.puts(x)
     IO.puts(@y) #10
+    IO.puts(:hello)
+    name = "Chris"
+    status = Enum.random([:gold, :silver, :bronze])
+
+    if status === :gold do
+      IO.puts("#{name} has a status of gold!")
+    else
+      IO.puts("#{name} does not have a status of gold")
+    end
   end
 end
