0854bef186d587f8bfa547208256d1b2d0127936
ChrisShep98 <shepardchristopher1@gmail.com>
2025-06-21 14:50:54 -0400

    variable example

--- lib/example.ex
+++ lib/example.ex
@@ -1,9 +1,19 @@
 defmodule Example do
   use Application
   alias UUID
+
+  @y 10
+  # ^ workaround for a const variable in Elixir
+
   def start(_type, _args) do
+    Example.main()
     IO.puts(:"Hello World")
-    IO.puts(UUID.uuid4())
     Supervisor.start_link([], strategy: :one_for_one)
+  end
+
+  def main do
+    x = 5
+    IO.puts(x)
+    IO.puts(@y) #10
   end
 end
