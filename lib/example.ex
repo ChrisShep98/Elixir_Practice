c867e68de1cc4bff735727c18462c9dd8fc2e4e8
ChrisShep98 <shepardchristopher1@gmail.com>
2025-07-09 17:58:34 -0400

    creating own functions + anonymous function example

--- lib/example.ex
+++ lib/example.ex
@@ -2,17 +2,29 @@ defmodule Example do
   use Application
   alias UUID
 
-  @y 10
-  # ^ workaround for a const variable in Elixir
-
   def start(_type, _args) do
     Example.main()
     Supervisor.start_link([], strategy: :one_for_one)
   end
 
+  def sum_and_average(numbers) do
+    sum = Enum.sum(numbers)
+    average = sum / Enum.count(numbers)
+    {sum, average}
+  end
+
+  def print_numbers(numbers) do
+    numbers |> Enum.join(" ") |> IO.puts()
+  end
+
   def main do
-    even_numbers = Enum.filter([1, 2, 3, 4, 5, 6], fn x -> rem(x, 2) == 0 end)
-    IO.inspect(even_numbers)  # [2, 4, 6]
+    numbers = [1, 2, 3, 4, 5]
+    Enum.each(numbers, fn num -> IO.puts(num) end)
+
+    new_numbers = ["1", "2", "3", "4", "5"]
+    result = Enum.map(new_numbers, &String.to_integer/1)
+    print_numbers(result)
+    IO.inspect(sum_and_average(result))
 
 
   end
